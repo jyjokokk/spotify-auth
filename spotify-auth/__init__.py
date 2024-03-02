7754fa6732d0fc456f3cb1791f07c444f7ea7e23
Jyrki Kokkola <jyrki.kokkola@outshine.fi>
2024-03-02 12:25:33 +0200

    init

--- spotify-auth/__init__.py
+++ spotify-auth/__init__.py
@@ -1,7 +1,9 @@
-# init
-def lol():
-    print("lol")
+# This is the __init__.py file
 
+# Import any modules or packages you need here
 
-def asd():
-    print("asd")
+# Define any global variables or constants here
+
+# Define any functions or classes here
+
+# This code will be executed when the package is imported
